0bcdfcf23336505b560c41cf3b28489677996e19
Simone <startae14@gmail.com>
2020-07-04 15:45:45 +0200

    add google font in the Head component and modify logo color and weight

--- components/PageLayout.js
+++ components/PageLayout.js
@@ -1,22 +1,29 @@
-import React from 'react';
+import Head from 'next/head';
+
 import { Container } from 'react-bootstrap';
 import BlogNavbar from './BlogNavbar';
 
 const PageLayout = ({ children, className }) => {
   return (
-    <Container>
-      <BlogNavbar />
-      <div className={`page-wrapper ${className}`}>
-        {children}
-      </div>    
-      <footer className="page-footer">
-        <div>
-          <a href="#">courses</a>{' | '}
-          <a href="#">github</a>{' | '}
-          <a href="#">facebook</a>
-        </div>
-      </footer>
-    </Container>
+    <>
+      <Head>
+        <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;700;900&display=swap" rel="stylesheet" />
+      </Head>
+      <Container>
+        <BlogNavbar />
+        <div className={`page-wrapper ${className}`}>
+          {children}
+        </div>    
+        <footer className="page-footer">
+          <div>
+            <a href="#">courses</a>{' | '}
+            <a href="#">github</a>{' | '}
+            <a href="#">facebook</a>
+          </div>
+        </footer>
+      </Container>
+    </>
+    
   );
 };
 
