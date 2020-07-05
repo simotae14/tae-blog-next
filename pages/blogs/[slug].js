b13e2e6d087dc68ed20cd5eb18f4bdda85855aff
Simone <startae14@gmail.com>
2020-07-05 22:02:10 +0200

    generate static pages for the 3 dynamic details pages

--- pages/blogs/[slug].js
+++ pages/blogs/[slug].js
@@ -5,6 +5,7 @@ import { getBlogBySlug } from 'lib/api';
 const BlogDetail = ({
   blog
 }) => {
+  console.log('Displaying page');
   return (
     <PageLayout>
       <h1>Detail page {blog?.slug}</h1>
@@ -12,13 +13,36 @@ const BlogDetail = ({
   );
 };
 
-export const getServerSideProps = async ({ params }) => {
+export const getStaticProps = async ({ params }) => {
+  console.log('Fetching blog by', params.slug);
   const blog = await getBlogBySlug(params.slug);
   return {
     props: {
       blog
     }
   }
-} 
+};
+
+export const getStaticPaths = () => {
+  console.log('getting paths for every page');
+  return {
+    paths: [{
+      params: {
+        slug: 'my-third-blog'
+      }
+    },
+    {
+      params: {
+        slug: 'my-second-blog'
+      }
+    },
+    {
+      params: {
+        slug: 'my-first-blog'
+      }
+    }],
+    fallback: false
+  }
+};
 
 export default BlogDetail;
