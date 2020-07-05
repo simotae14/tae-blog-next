bbda4fc1ccc6eb20b80c8b2cf5a5f9a46ffe44c7
Simone <startae14@gmail.com>
2020-07-06 00:23:40 +0200

    render the blog content with code andimages

---
+++ components/BlogContent.js
@@ -0,0 +1,36 @@
+import BlockContent from '@sanity/block-content-to-react';
+
+const serializers = {
+  types: {
+    code: ({
+      node: {
+        language,
+        code,
+        filename
+      }
+    }) => (
+      <pre data-language={language}>
+        <code>{code}</code>
+        <p>{filename}</p>
+      </pre>
+    )
+  }
+};
+
+const BlogContent = ({content}) => {
+  return (
+    <>
+      <BlockContent
+        imageOptions={{
+          w: 320,
+          h: 240,
+          fit: 'max'
+        }}
+        serializers={serializers}
+        blocks={content}
+      />
+    </>
+  );
+};
+
+export default BlogContent;
--- pages/blogs/[slug].js
+++ pages/blogs/[slug].js
@@ -1,5 +1,6 @@
 import PageLayout from 'components/PageLayout';
 import BlogHeader from 'components/BlogHeader';
+import BlogContent from 'components/BlogContent';
 import { Row, Col } from 'react-bootstrap';
 
 import { getBlogBySlug, getAllBlogs } from 'lib/api';
@@ -19,8 +20,9 @@ const BlogDetail = ({
             coverImage={blog?.coverImage}
           />
           <hr/>
-          {/* Blog Content Here */}
-          It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).
+          <BlogContent 
+            content={blog?.content}
+          />
         </Col>
       </Row>
     </PageLayout>
