f4d221b2db2856aaf92acef1e1d37cf27401d795
Simone <startae14@gmail.com>
2021-02-07 23:28:20 +0100

    create a Filtering menu button to change the layout of the Blog list in the home page

---
+++ components/FilteringMenu.js
@@ -0,0 +1,13 @@
+const FilteringMenu = ({ onChange }) => {
+  return (
+    <div className="filtering-menu mb-2">
+      <div onClick={() => {
+        onChange();
+      }}>
+        Change View
+      </div>
+    </div>
+  );
+}
+
+export default FilteringMenu;
--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,29 @@
+import { useState } from 'react';
 import { Row, Col } from 'react-bootstrap';
 import PageLayout from 'components/PageLayout';
 import AuthorIntro from 'components/AuthorIntro';
-import CardListItem from 'components/CardListItem';
 import CardItem from 'components/CardItem';
+import CardListItem from 'components/CardListItem';
+import FilteringMenu from 'components/FilteringMenu';
 
 import { getAllBlogs } from 'lib/api';
 
 const Home = ({ blogs }) => {
+  const [ filter, setFilter ] = useState({
+    view: {
+      list: 0
+    }
+  })
   return (
     <PageLayout>
       <AuthorIntro />
+      <FilteringMenu onChange={() => {
+        setFilter({
+          view: {
+            list: filter.view.list === 0 ? 1 : 0
+          }
+        })
+      }} />
       <hr/>
       <Row className="mb-5">
         {/*<Col md="10">
@@ -17,19 +31,26 @@ const Home = ({ blogs }) => {
         </Col>*/}
         {
           blogs.map(({ title, subtitle, date, coverImage, slug, author }) => (
-            <Col key={slug} md="4">
-              <CardItem
-                title={title}
-                subtitle={subtitle}
-                date={date}
-                image={coverImage}
-                author={author}
-                link={{
-                  href: "/blogs/[slug]",
-                  as: `/blogs/${slug}`
-                }}
-              />
-            </Col>
+            filter.view.list ?
+            (
+              <Col md="9">
+                <CardListItem />
+              </Col>
+            ) : (
+              <Col key={slug} md="4">
+                <CardItem
+                  title={title}
+                  subtitle={subtitle}
+                  date={date}
+                  image={coverImage}
+                  author={author}
+                  link={{
+                    href: "/blogs/[slug]",
+                    as: `/blogs/${slug}`
+                  }}
+                />
+              </Col>
+            )
           ))
         }
       </Row>
@@ -44,6 +65,6 @@ export const getStaticProps = async () => {
       blogs
     }
   }
-} 
+}
 
 export default Home;
